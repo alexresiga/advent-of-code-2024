8689d32fe9148bc7c47813fbe7f6aeea4bf4c92b
alexresiga <resiga.alex@gmail.com>
2024-12-01 11:17:28 +0200

    sum day one  📋

--- src/day01.kt
+++ src/day01.kt
@@ -1,3 +1,13 @@
-fun main() = day {_, lines ->
-    println(lines)
+fun main() = day(true, name = "--- Day 1: Historian Hysteria ---") { part, lines ->
+    val first = mutableListOf<Int>()
+    val second = mutableListOf<Int>()
+    lines.forEach { line ->
+        val (f, s) = line.split("   ").map { it.toInt() }
+        first.add(f)
+        second.add(s)
+    }
+    when (part) {
+        Part.One -> first.sorted().zip(second.sorted()).sumOf { (a, b) -> kotlin.math.abs(a - b) }.println()
+        Part.Two -> first.sumOf { f -> f * second.count { it == f } }.println()
+    }
 }
