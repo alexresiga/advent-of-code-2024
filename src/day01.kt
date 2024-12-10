52fe0fd19f49871e41d86cb38a6e5c55bb53c8cb
alexresiga <resiga.alex@gmail.com>
2024-12-11 00:59:29 +0200

    sum day one 📋

--- src/day01.kt
+++ src/day01.kt
@@ -1,6 +1,7 @@
 fun main() = day(true, name = "--- Day 1: Historian Hysteria ---") { part, lines ->
     val (first, second) = lines.map { line ->
-        val (f, s) = line.split("   ").map { it.toInt() }; f to s
+        val (f, s) = line.split("   ").map { it.toInt() }
+        f to s
     }.unzip()
     when (part) {
         Part.One -> first.sorted().zip(second.sorted()).sumOf { (f, s) -> kotlin.math.abs(f - s) }.println()
